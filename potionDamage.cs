729ae487e7b979a8a2a4a500807998fbcc6758b1
Hecateo <juniorcubamontero83@gmail.com>
2026-04-29 18:40:11 -0500

    potions
    life and damage

---
+++ potionDamage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupalT3d26._1
+{
+    internal class potionDamage
+    {
+        int DamageToEnemy;
+
+        public potionDamage(int damageToEnemy)
+        {
+            this.DamageToEnemy = damageToEnemy;
+            DañarEnemigo();
+        }
+
+        void DañarEnemigo()
+        {
+            DamageToEnemy = -2;
+        }
+    }
+}
