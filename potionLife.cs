bc566ad73a7f3c8bcfe914174a2bffd23c015d10
Hecateo <juniorcubamontero83@gmail.com>
2026-04-30 16:36:11 -0500

    Revert "potions"
    This reverts commit 8cc36f73972076f7d647b3df2c6bd59f24a98fef.

---
+++ potionLife.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupalT3d26._1
+{
+    internal class potionLife : Character
+    {
+        public potionLife(string name, int life, int damage) : base(name, life, damage)
+        {
+            life += 3;
+        }
+    }
+}
